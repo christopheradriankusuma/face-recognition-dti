29dd2f186bfa91f6d495da3c9a3d223999ef718b
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-09-12 18:19:09 +0700

    remove register from main

--- main.py
+++ main.py
@@ -39,10 +39,4 @@ def catatKehadiran():
     print("Record Face ID Pengguna")
     os.system('python catatpengunjung.py') #Ganti sesuai versi python
 
-@eel.expose
-def register(nama, nrp):
-    print("Register Pengguna")
-    print("Nama: {} | NRP: {}".format(nama, nrp))
-    os.system("python register.py {} {}".format(nama, nrp))
-
 eel.start('index.html', size=(1000, 600), mode='edge')
--- tampilan/main.js
+++ tampilan/main.js
@@ -5,9 +5,3 @@ function rekamwajah() {
 function catatKehadiran() {
 	eel.catatKehadiran()
 }
-
-function register(){
-	var nama = document.getElementById("nama").value;
-	var nrp = document.getElementById("nrp").value;
-	eel.register(nama,nrp)
-}
