0e53d4dad9b73059c87a595a7779b3c39a4a636d
Christopher Adrian Kusuma <32239110+christopheradriankusuma@users.noreply.github.com>
2021-09-17 16:11:03 +0700

    Update catatpengunjung.py

--- catatpengunjung.py
+++ catatpengunjung.py
@@ -148,7 +148,7 @@ while detecting:
     objek = nyalain.get_frame()
     rekamWajah = detector.detect(objek, False) #deteksi lebih dari satu wajah
     if timer == 0:
-        df = pd.read_csv(StringIO(requests.get("https://absensi-dti.herokuapp.com/hayo-ngapain-kesini-dti-9987b6e63716f1c918d5ed38fb7b3bd7").text), dtype={'NRP': object})
+        df = pd.read_csv(StringIO(requests.get(SECRET_URL).text), dtype={'NRP': object})
     timer = (timer + 1) % 1000
     #Cek Face Recognition
     if len(rekamWajah):
