d45d7e2f9863383cdf43b01ab210606c5bd2b76e
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-09-17 16:59:48 +0700

    Update register

--- app.py
+++ app.py
@@ -2,20 +2,13 @@ from flask import Flask, request, render_template
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-from flask.helpers import send_from_directory 
+from flask.helpers import send_from_directory
 import pandas as pd
 import re
 import random
 import string
-import csv
 import qrcode
 import os
-
-if not os.path.exists('database.csv'):
-    fields = ['ID', 'Nama', 'No Induk', 'Tanggal lahir', 'Token']
-    with open(r'database.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(fields)
 
 app = Flask(__name__)
 
@@ -27,6 +20,7 @@ def index():
 def register():
     nama = request.form.get('nama', '')
     nrp = request.form.get('nrp', '')
+    tgl = request.form.get('tgl-lahir', '')
 
     nama_valid = re.findall(r'[a-zA-Z\' ]+', nama)
     nrp_valid = re.findall(r'\d+', nrp)
@@ -35,7 +29,7 @@ def register():
         error = "Pastikan nama/NRP yang anda masukkan benar"
         return render_template('index.html', error=error)
 
-    if token:=tambah_user(nama, nrp):
+    if token:=tambah_user(nama, nrp, tgl):
         return render_template('index.html', img=f"images/{nrp}.png", token=token)
     else:
         return render_template('index.html', error="NRP telah terdaftar")
@@ -48,12 +42,16 @@ def download(nrp):
 # def db():
 #     return send_from_directory('', 'database.csv', as_attachment=True)
 
-def tambah_user(nama, nrp):
-    df = pd.read_csv('database.csv', dtype={'NRP': object})
+def tambah_user(nama, nrp, tgl):
+    df = pd.read_csv('database.csv', dtype={'No Induk': object})
 
     tokens = df['Token'].values
-    nrps = df['NRP'].values
-    if nrp in nrps:
+    print(tokens)
+    nrps = df['No Induk'].values
+    if nrp not in nrps:
+        return False
+
+    if df[df['No Induk'] == nrp]['Tanggal lahir'].values[0] != tgl:
         return False
 
     length = 6
@@ -72,9 +70,6 @@ def tambah_user(nama, nrp):
 
     qr = qrcode.make(token)
 
-    file = open("database.csv")
-    reader = csv.reader(file)
-    id = len(list(reader))
     qr.save(lokasi_file)
 
     image = Image.open(lokasi_file)
@@ -85,10 +80,7 @@ def tambah_user(nama, nrp):
     draw.text(((ukuran[0]//2)-32, (ukuran[1]*8.69)//10), token, fill=0, font=font)
 
     image.save(lokasi_file)
-
-    with open(r'database.csv', 'a', newline='') as f:
-        fields = [id, nama, nrp, token]
-        writer = csv.writer(f)
-        writer.writerow(fields)
+    df.loc[df['No Induk'] == nrp, ['Token']] = token
+    df.to_csv('database.csv', index=False)
 
     return token
